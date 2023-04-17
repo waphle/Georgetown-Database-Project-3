c5ab905577534f6e1f0586f3d1d5d29c5deb8d4b
Jeff Zhang <40436311+waphle@users.noreply.github.com>
2023-04-17 12:55:22 -0400

    Update btree_framework.py

--- btree_framework.py
+++ btree_framework.py
@@ -3,18 +3,18 @@ from sqlParser import SQL
 
 class Table:
     def __init__(self, name, PK) -> None:
-        self.tree = btree.BTree(1)
+        self.tree = btree.BTree(2)
         self.PK = PK
         self.name = name
 
     def __init__(self, name) -> None:
-        self.tree = btree.BTree(1)
+        self.tree = btree.BTree(2)
         self.name = name
 
     def insert_row(self, row):
         key = ""
         for k in self.PK:
-            k += "(" + row[k] + ")"
+            key += "(" + row[k] + ")"
         self.tree[key]= row
 
     def delete_row(self, key):
@@ -27,14 +27,30 @@ def operation(sql):
     if sql.operation == "CREATE":
         test = sql # Code here
         
-def insertion(btree, sqlParser, sql):
+def insertion(btree, sql):
     btree = BTree()
     sql = "INSERT INTO my_table (key, value) VALUES (1, 'one')"
     insertion(btree, sql)
     
-    query = sqlParser(sql)
+    query = SQL(sql)
 
     key = query['values'][0]
     value = query['values'][1]
 
     btree.set(key, value)
+
+def delete(btree, sql):
+    query = SQL(sql)
+    key = query['where'][0]['value']
+
+    # Delete the key-value pair from the BTree
+    btree.delete(key)
+
+    btree = BTree()
+    btree.set(1, 'one')
+    btree.set(2, 'two')
+    sql = "DELETE FROM my_table WHERE key = 1"
+    delete(btree, sql)
+
+def select(btree, sql):
+    
