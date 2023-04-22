e8a1fa42b84e3c3db3cc10c45f98fd43a5afd9c0
Jeff Zhang <40436311+waphle@users.noreply.github.com>
2023-04-22 14:34:51 -0400

    Update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,48 @@
-# Tasks:
-# 1. The optimizer for Conjunctive and disjunctive condition ordering 
-# is to base on the conditions, and 
-# 2. To reorder the and and or
+from sqlParser import SQL
+
+class Condition:
+    def __init__(self, column, operator, value, sqlParser):
+        self.column = column
+        self.operator = operator
+        self.value = value
+        self.sqlParser = ""
+
+    def __repr__(self, sqlParser):
+        return f"{self.column} {self.operator} {self.value}"
+
+
+def RBO(conditions, sqlParser):
+    """
+    Optimize the order of conditions based on a predefined set of rules.
+    Args:
+        conditions (list): List of Condition objects.
+
+    Returns:
+        list: Ordered list of Condition objects.
+    """
+
+    # Define custom sorting rules
+    def sort_key(condition):
+        # Rule 1: prioritize equality conditions
+        if condition.operator == "=":
+            return 1
+        # Rule 2: prioritize range conditions (less than, greater than)
+        elif condition.operator in ("<", ">", "<=", ">="):
+            return 2
+        # Rule 3: prioritize other conditions (like, etc.)
+        else:
+            return 3
+
+    return sorted(conditions, key=sort_key)
+
+
+# Example usage
+conditions = [
+    Condition("column1", ">", "100"),
+    Condition("column2", "=", "50"),
+    Condition("column3", "LIKE", "'%value%'")
+]
+
+optimized_conditions = RBO(conditions)
+
+print("Optimized Condition Order:", optimized_conditions)
