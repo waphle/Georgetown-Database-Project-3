32a9633cff0922a47c7d57a76b9bf88e8e0f133a
etanger <qq444603907@gmail.com>
2023-04-27 11:44:15 -0400

    done all function

--- demo_sql.py
+++ demo_sql.py
@@ -1,19 +1,60 @@
 # Generate random SQL queries for demo
-import sqlparse
-import sqlParser
-import random
+import btree_framework
 from sqlParser import SQL
 
-def generator(self, sqlParser):
-    table = ['customers', 'orders', 'products']
-    column = ['id', 'name', 'price', 'quantity']
-    num_queries = 10
 
-    for i in range(num_queries):
-        rand_table = random.choice(table)
-        num_column = random.randint(1, len(column))
-        selected_column = random.sample(column, num_column)
+def generator():
 
-        sql_query = f"SELECT {', '.join(selected_column)} FROM {rand_table};"
+    sql = "CREATE TABLE Rel_i_i_1000 ( c1 INT PRIMARY KEY, c2 INT );"
+    btree_framework.operation(SQL(sql))
+    sql = "INSERT INTO Rel_i_i_1000 (c1, c2) VALUES "
+    for i in range(1, 1001):
+        if i != 1:
+            sql += ","
+        sql += "({}, {})".format(i, i)
+    btree_framework.operation(SQL(sql))
 
-    print(sql_query)
+    sql = "CREATE TABLE Rel_i_i_10000 ( c1 INT PRIMARY KEY, c2 INT );"
+    btree_framework.operation(SQL(sql))
+    sql = "INSERT INTO Rel_i_i_10000 (c1, c2) VALUES "
+    for i in range(1, 10001):
+        if i != 1:
+            sql += ","
+        sql += "({}, {})".format(i, i)
+    btree_framework.operation(SQL(sql))
+
+    sql = "CREATE TABLE Rel_i_i_100000 ( c1 INT PRIMARY KEY, c2 INT );"
+    btree_framework.operation(SQL(sql))
+    sql = "INSERT INTO Rel_i_i_100000 (c1, c2) VALUES "
+    for i in range(1, 100001):
+        if i != 1:
+            sql += ","
+        sql += "({}, {})".format(i, i)
+    btree_framework.operation(SQL(sql))
+
+    sql = "CREATE TABLE Rel_i_1_1000 ( c1 INT PRIMARY KEY, c2 INT );"
+    btree_framework.operation(SQL(sql))
+    sql = "INSERT INTO Rel_i_1_1000 (c1, c2) VALUES "
+    for i in range(1, 1001):
+        if i != 1:
+            sql += ","
+        sql += "({}, {})".format(i, 1)
+    btree_framework.operation(SQL(sql))
+
+    sql = "CREATE TABLE Rel_i_1_10000 ( c1 INT PRIMARY KEY, c2 INT );"
+    btree_framework.operation(SQL(sql))
+    sql = "INSERT INTO Rel_i_1_10000 (c1, c2) VALUES "
+    for i in range(1, 10001):
+        if i != 1:
+            sql += ","
+        sql += "({}, {})".format(i, 1)
+    btree_framework.operation(SQL(sql))
+
+    sql = "CREATE TABLE Rel_i_1_100000 ( c1 INT PRIMARY KEY, c2 INT );"
+    btree_framework.operation(SQL(sql))
+    sql = "INSERT INTO Rel_i_1_100000 (c1, c2) VALUES "
+    for i in range(1, 100001):
+        if i != 1:
+            sql += ","
+        sql += "({}, {})".format(i, 1)
+    btree_framework.operation(SQL(sql))
