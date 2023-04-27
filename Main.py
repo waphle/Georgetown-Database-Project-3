6e1edc8930dca3678649eb40973cbc7f4bac1633
Baron Du <2467062528@qq.com>
2023-04-27 11:44:42 -0400

    On branch Yunqi_Branch Changes to be committed: 	modified:   Main.py 	modified:   Redis_test.py Changes not staged for commit: 	modified:   btree_framework.py Untracked files: 	__pycache__/Redis_test.cpython-310.pyc 	__pycache__/btree_framework.cpython-310.pyc 	__pycache__/parser_test.cpython-310.pyc 	__pycache__/sqlParser.cpython-310.pyc

--- Main.py
+++ Main.py
@@ -4,7 +4,6 @@ import Redis_test
 import parser_test
 from redis import StrictRedis
 
-
 db = StrictRedis(host='127.0.0.1', port=6379, db=0)
 db.flushdb()
 sql = "CREATE TABLE example_table (id INT, grade INT);"
@@ -13,6 +12,20 @@ sql = "ALTER TABLE example_table ADD PRIMARY KEY (id);"
 btree_framework.operation(SQL(sql))
 sql = "INSERT INTO example_table (id, grade) VALUES (1, 10), (2, 20), (3, 15), (4, 5), (5, 30);"
 btree_framework.operation(SQL(sql))
+Redis_test.data_store(db, btree_framework.databases["example_table"], "example_table")
 while True:
-    sql = str(input("Input your sql command:\n"))
-    btree_framework.operation(SQL(sql))
+    sql = SQL(str(input("Input your sql command:\n")))
+    if sql.operation.upper() == 'SELECT':
+        databases = Redis_test.data_load(db, sql.table)
+    btree_framework.operation(sql)
+
+    if sql.operation.upper() == 'DROP':
+        if db.exists(sql.table):
+            db.delete(sql.table)
+
+    if sql.operation.upper() == 'INSERT' or sql.operation.upper() == 'UPDATE' or sql.operation.upper() == 'DELETE':
+        if db.exists(sql.table):
+            db.delete(sql.table)
+        Redis_test.data_store(db, btree_framework.databases[sql.table], sql.table)
+
+
